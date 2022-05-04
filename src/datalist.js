f5bdab5916dd0a347305e91f2d4e2de46d332cf5
jlayog <35618625+jlayog@users.noreply.github.com>
2022-05-04 15:30:33 -0400

    Update datalist.js

--- src/datalist.js
+++ src/datalist.js
@@ -1,5 +1,5 @@
 export const messages = [
-  "Hi, I am Jerel Layog. I am a Frontend Developer.",
+  "Hi, I am Jerel Layog. I am a Frontend Web Developer.",
   "I love creating software and web apps.",
   "Looking to hire?",
   "Check out my work."
